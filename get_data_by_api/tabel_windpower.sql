b5a41ab4928150981ad1a8b9cc6df745ca23ca10
2626060z <2626060z@student.gla.ac.uk>
2022-10-26 16:49:41 +0100

    upload windpower sql script and the sample data getting from the wind power restful api

---
+++ get_data_by_api/tabel_windpower.sql
@@ -0,0 +1,27 @@
+SHOW DATABASES;
+CREATE database sh33;
+SHOW DATABASES;
+
+USE sh33;
+-- The following tabel is created based the returned fields getting from the Wind Power Restful API.
+CREATE TABLE windpower(
+	time_series_id TEXT,
+    registered_resource_eic_code TEXT,
+	bm_unit_id TEXT,
+	ngc_bm_unit_id TEXT,
+    psr_type TEXT,
+	market_generation_unit_eic_code TEXT,
+	market_generation_bm_unit_id TEXT,
+	market_generation_ngc_bm_unit_id TEXT,
+	settlement_date DATE,
+	settlement_period INT,
+    quantity TEXT
+);
+
+SELECT * FROM windpower;
+
+-- insert one piece of data with following command
+-- INSERT INTO windpower(time_series_id, registered_resource_eic_code, bm_unit_id, ngc_bm_unit_id, psr_type, market_generation_unit_eic_code, market_generation_bm_unit_id, market_generation_ngc_bm_unit_id, settlement_date, settlement_period, quantity) 
+-- VALUES('ELX-EMFIP-AGOG-TS-219', '48W00000GLOFW-15', 'E_GLOFW-1', 'GLOFW-1', 'Generation', '48W00000GLOFW-15', 'E_GLOFW-1', 'GLOFW-1', '2022/1/1',  1, '1.202');
+-- SELECT * FROM windpower;
+
