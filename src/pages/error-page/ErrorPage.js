4f72207f3312c01f58d722c9e09ffa08bb6688fe
George Ntogramatzis <2518439n@student.gla.ac.uk>
2022-11-02 13:21:01 +0000

    created error page and added it to router

---
+++ src/pages/error-page/ErrorPage.js
@@ -0,0 +1,25 @@
+import { useRouteError } from "react-router-dom";
+import { NavBar } from "../../components";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <>
+      <NavBar />
+      <div className="text-center">
+        <h1 className="mt-5 display-1">{error.status}</h1>
+        <h1 className="mt-5 display-3">{error.statusText || error.message}</h1>
+        {error.status === 404 && (
+          <h3 className="mt-5 display-5">
+            The page you are looking for doesn’t exist. <br /> Go back or start
+            afresh at sitename.com
+          </h3>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ErrorPage;
