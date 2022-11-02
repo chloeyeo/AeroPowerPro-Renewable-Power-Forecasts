09bc8143ca7575b1af44cb3c61514a24a2bc01c6
George Ntogramatzis <2518439n@student.gla.ac.uk>
2022-11-02 12:01:56 +0000

    Revert "Merge branch 'setup-bootstrap' into 'main'"
    This reverts merge request !5

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,10 +1,5 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 
-const About = () => (
-  <div>
-    <Button variant="primary">About page</Button>
-  </div>
-);
+const About = () => <div>About page</div>;
 
 export default About;
