b9bfe3172c657d9276a1c5796e6e99e9921a8acd
Leonidas Ioannou <2454882I@student.gla.ac.uk>
2022-11-02 13:54:50 +0000

    Improved navbar: https://stgit.dcs.gla.ac.uk/team-project-h/2022/sh33/sh33-main/-/issues/67

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,32 +1,49 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "react-bootstrap";
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar style={{ backgroundColor: "#373b44" }} expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Jehtro's Power Forecast</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Signup/Login</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+        <Navbar.Brand href="/" style={{ color: "white" }}>
+          Jehtro's Power Forecast
+        </Navbar.Brand>
+        <div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/" style={{ color: "white" }}>
+                Home
+              </Nav.Link>
+              <Nav.Link href="/signup" style={{ color: "white" }}>
+                Signup/Login
+              </Nav.Link>
+              <Nav.Link href="/about" style={{ color: "white" }}>
+                About
+              </Nav.Link>
+              <NavDropdown
+                title={<span style={{ color: "white" }}>Dropdown</span>}
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
