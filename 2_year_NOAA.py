4d333738054a9a796bbd3032f3b291beec769036
Christoforos Mylona <2552874m@student.gla.ac.uk>
2022-11-02 13:34:48 +0000

    Adding to Historic Wind DB now works, tables will be refactored

--- 2_year_NOAA.py
+++ 2_year_NOAA.py
@@ -3,6 +3,8 @@ import netCDF4 as nc
 import datetime
 from dateutil.relativedelta import relativedelta
 import numpy as np
+from insert_to_historic_wind import insert_historic_wind
+
 today = datetime.datetime.today() 
 dat = datetime.datetime.today()
 dat = dat - relativedelta(years=1)
@@ -19,13 +21,11 @@ def get_historic_wind_all():
         month = dat.strftime("%m")
         day = dat.strftime('%d')
         hour = dat.strftime("%H")
-        # dat += relativedelta(hours = 6)
         url = f"https://www.ncei.noaa.gov/thredds/ncss/model-gfs-004-files/{year}{month}/{year}{month}{day}/gfs_3_{year}{month}{day}_{hour}{minutes}_000.grb2?var=u-component_of_wind_altitude_above_msl&var=v-component_of_wind_altitude_above_msl&north=59&west=-7&east=3&south=-50&disableProjSubset=on&horizStride=1&time_start={year}-{month}-{day}T{minutes}%3A00%3A00Z&time_end=2022-09-30T18%3A00%3A00Z&timeStride=1&vertCoord="
         req = requests.get(url=url)
         if (req.status_code == 200):
             data = req.content
             link = nc.Dataset('anynamehere', memory = data)
-
 
             vals = list(link.variables.values())
             u_comp = np.array(vals[0])
@@ -42,14 +42,12 @@ def get_historic_wind_all():
                             insert_h = dat + relativedelta(hours = time[h])
                             insert_u = u_comp[h][msl][lat][lon]
                             insert_v = v_comp[h][msl][lat][lon]
-                            # if (insert_u is None or insert_u is None):
-                            #     print(dat.date, time[h])
-                            print(insert_u, insert_v, lats[lat], longs[lon], insert_h)
-
+                            insert_historic_wind(time = insert_h, msl = alt_msl[msl], lat = lats[lat], long = longs[lon], u_comp = insert_u, v_comp = insert_v)
+            print("ASD")
             # print(dat)
         else:
             print(req)
-
+        # break
         dat = dat + relativedelta(hours = 6)
     print("Done!")
 
--- insert_to_historic_wind.py
+++ insert_to_historic_wind.py
@@ -5,7 +5,7 @@ def insert_historic_wind(time, msl, lat, long, u_comp, v_comp):
     con_str = (
         'DRIVER=MySQL ODBC 8.0 ANSI Driver;'
         'SERVER=localhost;'
-        'DATABASE=localhost;'
+        'DATABASE=sh33;'
         'UID=root;'
         f'PWD={pwd};'
         'charset=utf8mb4;'
@@ -15,6 +15,16 @@ def insert_historic_wind(time, msl, lat, long, u_comp, v_comp):
     cnxn.setdecoding(db.SQL_WCHAR, encoding='utf-8')
     cnxn.setencoding(encoding='utf-8')
 
+    cursor = cnxn.cursor()
     
+    insert_list = [str(time.date()),str(time.time()) , str(msl), str(lat), str(long), str(u_comp), str(v_comp)]
+    # print(insert_list)
+    insert_sql = "INSERT INTO historic_wind(wind_date, wind_time, u_comp, v_comp, " \
+                 "msl, longitude, latitude) "
 
+    insert_list = ['"' + insert_list[i] + '"' for i in range(len(insert_list))]
+    cursor.execute(insert_sql + "values (" + ','.join(insert_list) + ");")
+    cnxn.commit()
+
+    
 
