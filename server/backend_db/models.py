bd677c0caab8d34ff50d1b61a6eaa01b3d2eaa65
Christoforos Mylona <2552874m@student.gla.ac.uk>
2022-11-09 14:07:10 +0000

    Fixed Historic wind table

--- server/backend_db/models.py
+++ server/backend_db/models.py
@@ -3,9 +3,10 @@ from ELEXON_models import ActualProduceElectricity
 # Create your models here.
 
 class HistoricWind(models.Model):
-    wind_data_id = models.IntegerField(primary_key = True, auto_created = True)
-    msl = models.IntegerField()
-    date_val = models.DateTimeField()
-    longitude = models.IntegerField()
-    latitute = models.IntegerField()
-    u_comp = models.FloatField()
+    wind_data_id = models.IntegerField(primary_key = True, unique = True, auto_created = True)
+    height_above_ground = models.IntegerField(blank = False)
+    date_val = models.DateTimeField(blank = False)
+    longitude = models.IntegerField(blank = False)
+    latitute = models.IntegerField(blank = False)
+    u_comp = models.FloatField(blank = False)
+    v_comp = models.FloatField(blank = False)
