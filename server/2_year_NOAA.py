d5cf01c6c24118f94b1045fa7e9190e3afc1de33
Christoforos Mylona <2552874m@student.gla.ac.uk>
2022-11-09 14:02:36 +0000

    Created population script for NOAA + admin page setup

--- server/2_year_NOAA.py
+++ server/2_year_NOAA.py
@@ -4,11 +4,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 import django
 django.setup()
 from backend_db.models import HistoricWind
-from django.utils.timezone import now
+# from django.utils.timezone import now
+import datetime
 
 import requests
 import netCDF4 as nc
-import datetime
 from dateutil.relativedelta import relativedelta
 import numpy as np
 
@@ -23,34 +23,44 @@ def split_date(dat):
 
 def split_net(link):
     vals = list(link.variables.values())
-    u_comp = np.array(vals[0])
-    time = np.array(vals[1])
-    alt_msl = np.array(vals[2])
-    lats = np.array(vals[3])
+    # time = np.array(vals[0])
+    lats = np.array(vals[1])
+    heights = np.array(vals[2])
+    time = np.array(vals[3])
     longs = np.array(vals[4])
-    v_comp = np.array(vals[-1])
+    u_comp = np.array(vals[5])
+    v_comp = np.array(vals[6])
 
-    return u_comp, time, alt_msl, lats, longs, v_comp
+    return u_comp, time, heights, lats, longs, v_comp
 
 
 def historic_wind_pull(dat):
     year, month, day, hour, minutes = split_date(dat)
-    url = f"https://www.ncei.noaa.gov/thredds/ncss/model-gfs-004-files/{year}{month}/{year}{month}{day}/gfs_3_{year}{month}{day}_{hour}{minutes}_000.grb2?var=u-component_of_wind_altitude_above_msl&var=v-component_of_wind_altitude_above_msl&north=59&west=-7&east=3&south=-50&disableProjSubset=on&horizStride=1&time_start={year}-{month}-{day}T{minutes}%3A00%3A00Z&time_end=2022-09-30T18%3A00%3A00Z&timeStride=1&vertCoord="
+    # url = f"https://www.ncei.noaa.gov/thredds/ncss/model-gfs-004-files/{year}{month}/{year}{month}{day}/gfs_3_{year}{month}{day}_{hour}{minutes}_000.grb2?var=u-component_of_wind_altitude_above_msl&var=v-component_of_wind_altitude_above_msl&north=59&west=-7&east=3&south=-50&disableProjSubset=on&horizStride=1&time_start={year}-{month}-{day}T{minutes}%3A00%3A00Z&time_end={year}-{month}-{day}T18%3A00%3A00Z&timeStride=1&vertCoord="
+    # print("Year:", year, "\tmonth: ", month, "\tday: ", day)
+    # print(url)
+    url = f"https://www.ncei.noaa.gov/thredds/ncss/grid/model-gfs-004-files/{year}{month}/{year}{month}{day}/gfs_3_{year}{month}{day}_{hour}{minutes}_000.grb2?var=u-component_of_wind_height_above_ground&var=v-component_of_wind_height_above_ground&north=59&west=-3&east=4&south=50&horizStride=1&time_start={year}-{month}-{day}T{hour}:{minutes}:00Z&time_end={year}-{month}-{day}T{hour}:{minutes}:00Z&timeStride=1&&accept=netcdf3"
     return requests.get(url=url)
 
 
 def historic_wind_insert(link, dat):
-    u_comp, time, alt_msl, lats, longs, v_comp = split_net(link)
+    u_comp, time, heights, lats, longs, v_comp = split_net(link)
 
     for h in range(len(time)):
         insert_h = dat + relativedelta(hours = time[h])
-        for msl in range(len(alt_msl)):
+        for height in range(len(heights)):
             for lat in range(len(lats)):
                 for lon in range(len(longs)):
-                    insert_u = u_comp[h][msl][lat][lon]
-                    insert_v = v_comp[h][msl][lat][lon]
+                    insert_u = u_comp[h][height][lat][lon]
+                    insert_v = v_comp[h][height][lat][lon]
                     # insert_historic_wind(time = insert_h, msl = alt_msl[msl], lat = lats[lat], long = longs[lon], u_comp = insert_u, v_comp = insert_v)
-                    new_historic_wind = HistoricWind.objects.get_or_create(date_val = insert_h, msl = alt_msl[msl], lat = lats[lat], long = longs[lon], u_comp = insert_u, v_comp = insert_v)
+                    new_historic_wind = HistoricWind.objects.get_or_create(date_val = insert_h, 
+                                                                            height_above_ground = heights[height], 
+                                                                            latitute = lats[lat], 
+                                                                            longitude = longs[lon], 
+                                                                            u_comp = insert_u, 
+                                                                            v_comp = insert_v)[0]
+                    new_historic_wind.save()
 
 
 def historic_wind_pull_insert(dat):
@@ -65,9 +75,9 @@ def historic_wind_pull_insert(dat):
 
 
 def get_historic_wind_all():
-    today = now().date()
+    today = datetime.datetime.now()
     dat = today - relativedelta(years=2)
-    dat = dat.replace(hour = 00, minute = 00, second = 0, microsecond = 0)
+    dat = dat.replace(hour = 00, minute = 00, second = 0, microsecond= 0)
     test_dat = today - relativedelta(years = 1)
     test_dat = test_dat - relativedelta(months = 11)
 
--- server/backend_db/admin.py
+++ server/backend_db/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from backend_db.models import HistoricWind
 # Register your models here.
+
+admin.site.register(HistoricWind)
