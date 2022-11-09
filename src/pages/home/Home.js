28ccfc1c33b9afdd98485e4a8332cdd514d31955
Chloe Yeo <2458600y@student.gla.ac.uk>
2022-11-09 11:15:40 +0000

    map component created and code for map component added but now commented and map initialised to empty array to prevent map method being called before the data from an API request has arrived https://stgit.dcs.gla.ac.uk/team-project-h/2022/sh33/sh33-main/-/issues/77

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { NavBar } from "components";
-import { SearchBar } from "./components";
+import { SearchBar, Map } from "./components";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 const Home = () => (
   <div>
     <NavBar />
     <SearchBar />
+    <Map />
   </div>
 );
 
-<Wrapper apiKey={"AIzaSyD3Q64w0HbCYo-Kl80P-Wbdenv13vSN_U8"} render={Home}>
-  <YourComponent />
+const render = (status: Status) => {
+  return <h1>{status}</h1>;
+};
+
+<Wrapper apiKey={"AIzaSyDIkYrb1DecstIMYjfK9uwzve-g1xFWpxw"} render={render}>
+  <Home />
 </Wrapper>;
 
 export default Home;
