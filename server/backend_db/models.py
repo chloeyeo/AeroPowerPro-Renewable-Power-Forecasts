79fac164f5d4ca3bed6effa7c1f545be1cfca5e5
Christoforos Mylona <2552874m@student.gla.ac.uk>
2022-11-09 11:18:25 +0000

    Fixed issues with django

--- server/backend_db/models.py
+++ server/backend_db/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-import ELEXON_models
+from . import ELEXON_models
 # Create your models here.
 
 class historic_wind(models.Model):
-    wind_data_id = models.IntegerField(primary_key = True, auto_create = True)
+    wind_data_id = models.IntegerField(primary_key = True, auto_created = True)
     msl = models.IntegerField()
     date_val = models.DateTimeField()
     longitude = models.IntegerField()
