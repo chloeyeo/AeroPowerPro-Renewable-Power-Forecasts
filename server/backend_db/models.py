88356d40f4205aa25bcf745105d517a6be8da800
Chloe Yeo <2458600y@student.gla.ac.uk>
2022-11-23 13:04:56 +0000

    current changes

--- server/backend_db/models.py
+++ server/backend_db/models.py
@@ -18,5 +18,6 @@ class UserProfile(models.Model):
     # The additional attributes we wish to include.
     website = models.URLField(blank=True)
     picture = models.ImageField(upload_to='profile_images', blank=True)
+    email = models.EmailField(max_length=254)
     def __str__(self):
         return self.user.username
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,43 @@
 import React from "react";
 import { NavBar } from "../../components";
 
+var ValidatingForm = React.createClass({
+  registerField: function(field) {        
+      var fields = this.state.fields;
+      fields.push(field);
+      this.setState({fields: fields});
+  },
+  submitForm: function(e) {
+      e.preventDefault();
+      var validForm = true;
+      this.state.fields.forEach(function(field) {
+          // just to make sure we don't crash if the field is missing the validation method
+          if(typeof field.isValid === "function") {
+              var validField = field.isValid();
+              validForm = validForm && validField;
+          }
+      });
+      if(validForm) {
+          console.log("form is valid");
+          // submit the form
+      } else {
+          console.log("form is invalid");
+          // don't submit the form :)
+      }
+  },
+  getInitialState: function() {
+      return {fields: []};
+  },
+render: function() {
+      var newChildren = React.Children.map(this.props.children, function(child) {
+          // adding our on componentMounted callback
+          newChild = React.cloneElement(child, {onComponentMounted: this.registerField});
+          return newChild;
+      }, this);
+  return (<form onSubmit={this.submitForm}>{newChildren}</form>);
+}
+});
+
 const Login = () => (
   <>
     <div>
@@ -16,7 +53,7 @@ const Login = () => (
       }}
     >
       <h1 className="pt-5 text-white text-center">Welcome</h1>
-      <form>
+      <<ValidatingForm id="loginForm">
         <div class="p-3 form-group">
           <label className="text-white" for="exampleInputEmail1">
             Email address
@@ -43,11 +80,11 @@ const Login = () => (
         <button type="submit" class="w-100 mt-4 btn btn-light">
           Submit
         </button>
-      </form>
+      </ValidatingForm>
       <h5 className="mt-4 text-center text-white">
         Don't have an account yet?
       </h5>
-      <button type="submit" class="w-100 btn text-white">
+      <button onclick="register()" type="submit" class="w-100 btn text-white">
         Register
       </button>
     </div>
