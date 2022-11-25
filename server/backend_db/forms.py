fb3a36bc344ad04451e2f5263d2c92c4305fd51d
2626060z <2626060z@student.gla.ac.uk>
2022-11-25 15:33:06 +0000

    add files for middleware for elexon and login page

--- server/backend_db/forms.py
+++ server/backend_db/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from backend_db.models import UserProfile
+from models import UserProfile, ActualProduceElectricity
 
 
 class UserForm(forms.ModelForm):
@@ -8,7 +8,16 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password',)
+
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('website', 'picture',)
+
+
+class ElexonForm(forms.ModelForm):
+    class Meta:
+        model = ActualProduceElectricity
+        fields = ('time_series_id', 'registed_resource_eic_code', 'bm_unit_id', 'ngc_bm_unit_id', 'psr_type', 'market_generation_unit_eic_code',
+                  'market_generation_bmu_id', 'market_generation_ngc_bmu_id', 'settlement_date', 'period', 'quantity')
