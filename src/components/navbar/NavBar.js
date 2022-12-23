871a99a16d78d59e21d608e4fae7dcad96658405
George Ntogramatzis <2518439n@student.gla.ac.uk>
2022-12-23 12:46:07 +0200

    https://stgit.dcs.gla.ac.uk/team-project-h/2022/sh33/sh33-main/-/issues/116

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -2,7 +2,6 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import "react-bootstrap";
 
 function NavBar() {
@@ -25,22 +24,6 @@ function NavBar() {
               <Nav.Link href="/contactus" style={{ color: "white" }}>
                 Contact Us
               </Nav.Link>
-              <NavDropdown
-                title={<span style={{ color: "white" }}>Dropdown</span>}
-                id="basic-nav-dropdown"
-              >
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
               <Nav.Link href="/login" style={{ color: "white" }}>
                 Login
               </Nav.Link>
