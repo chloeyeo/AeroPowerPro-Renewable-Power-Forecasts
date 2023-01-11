ab8254a6a7a9234b12291d810d518d731064ff72
Leonidas Ioannou <2454882I@student.gla.ac.uk>
2023-01-11 11:26:12 +0000

    added react router Links in navbar to make website navigation faster https://stgit.dcs.gla.ac.uk/team-project-h/2022/sh33/sh33-main/-/issues/127

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,46 +3,74 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function NavBar() {
-  const links = [
-    {
-      display: "Home",
-      link: "/",
-    },
-    {
-      display: "About",
-      link: "/about",
-    },
-    {
-      display: "Contact Us",
-      link: "/contactus",
-    },
-    {
-      display: "Login",
-      link: "/login",
-    },
-    {
-      display: "Register",
-      link: "/register",
-    },
-  ];
-
   return (
     <Navbar style={{ backgroundColor: "#373b44" }} expand="lg">
       <Container>
         <Navbar.Brand href="/" style={{ color: "white" }}>
-          Jethro's Power Forecast
+          Jethro's Power Forecasts
         </Navbar.Brand>
         <div>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              {links.map(({ display, link }) => (
-                <Nav.Link key={display} href={link} style={{ color: "white" }}>
-                  {display}
-                </Nav.Link>
-              ))}
+              <Link
+                to="/"
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                  fontSize: 20,
+                  paddingRight: 15,
+                }}
+              >
+                Home
+              </Link>
+              <Link
+                to="/about"
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                  fontSize: 20,
+                  paddingRight: 15,
+                }}
+              >
+                About
+              </Link>
+              <Link
+                to="/contactus"
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                  fontSize: 20,
+                  paddingRight: 15,
+                }}
+              >
+                Contact Us
+              </Link>
+              <Link
+                to="/login"
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                  fontSize: 20,
+                  paddingRight: 15,
+                }}
+              >
+                Login
+              </Link>
+              <Link
+                to="/register"
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                  fontSize: 20,
+                  paddingRight: 15,
+                }}
+              >
+                Register
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </div>
