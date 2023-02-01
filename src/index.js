38379a23ce1ca8131f111bf123a13df1f026896c
George Ntogramatzis <2518439n@student.gla.ac.uk>
2023-02-01 17:08:50 +0000

    https://stgit.dcs.gla.ac.uk/team-project-h/2022/sh33/sh33-main/-/issues/156

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
---
+++ src/pages/home/utils/index.js
@@ -0,0 +1,3 @@
+import geoJsonObject from "./geoJsonObject";
+
+export { geoJsonObject };
