c0fb7bf393564496927b401172da7f087b260aab
Leonidas Ioannou <2454882I@student.gla.ac.uk>
2023-02-01 14:36:52 +0000

    added windfarm detail script to population script https://stgit.dcs.gla.ac.uk/team-project-h/2022/sh33/sh33-main/-/issues/151

--- server/create_farm_data_db/Populate_windfarm_detail_data.py
+++ server/create_farm_data_db/Populate_windfarm_detail_data.py
@@ -29,44 +29,45 @@ def is_onshore(x):
         return True
     return False
 
-attributes_fp = "https://osuked.github.io/Power-Station-Dictionary/attribute_sources/renewable-energy-planning-database/renewable-energy-planning-database-q2-june-2021.csv"
-wind_query = QueryWindTurbineDetailRefactor(attributes_fp)
+def main():
+    attributes_fp = "https://osuked.github.io/Power-Station-Dictionary/attribute_sources/renewable-energy-planning-database/renewable-energy-planning-database-q2-june-2021.csv"
+    wind_query = QueryWindTurbineDetailRefactor(attributes_fp)
 
-for row in wind_query.df_attrs.itertuples():
-    data_dict = {}
-    data_dict['operator'] = row[1]
-    data_dict['sitename'] = row[2]
-    data_dict['is_onshore'] = is_onshore(row[3])
-    if(not math.isnan(row[4])):
-        data_dict['turbine_height'] = row[4]
-    else:
-        data_dict['turbine_height'] = None
-    if(not math.isnan(row[5])):
-        data_dict['number_of_turbines'] = row[5]
-    else:
-        data_dict['number_of_turbines'] = None
-    if(not math.isnan(row[6])):
-        data_dict['turbine_capacity'] = row[6]
-    else:
-        data_dict['turbine_capacity'] = None
-    
-    data_dict['development_status'] = row[7]
-    if(not math.isnan(row[8])):
-        data_dict['x_coordinate'] = row[8]
-    else:
-        data_dict['x_coordinate'] = None
-    if(not math.isnan(row[9])):
-        data_dict['y_coordinate'] = row[9]
-    else:
-        data_dict['y_coordinate'] = None
-    if(not math.isnan(row[10])):
-        data_dict['longitude'] = row[10]
-    else:
-        data_dict['longitude'] = None
-    if(not math.isnan(row[11])):
-        data_dict['latitude'] = row[11]
-    else:
-        data_dict['latitude'] = None
+    for row in wind_query.df_attrs.itertuples():
+        data_dict = {}
+        data_dict['operator'] = row[1]
+        data_dict['sitename'] = row[2]
+        data_dict['is_onshore'] = is_onshore(row[3])
+        if(not math.isnan(row[4])):
+            data_dict['turbine_height'] = row[4]
+        else:
+            data_dict['turbine_height'] = None
+        if(not math.isnan(row[5])):
+            data_dict['number_of_turbines'] = row[5]
+        else:
+            data_dict['number_of_turbines'] = None
+        if(not math.isnan(row[6])):
+            data_dict['turbine_capacity'] = row[6]
+        else:
+            data_dict['turbine_capacity'] = None
+        
+        data_dict['development_status'] = row[7]
+        if(not math.isnan(row[8])):
+            data_dict['x_coordinate'] = row[8]
+        else:
+            data_dict['x_coordinate'] = None
+        if(not math.isnan(row[9])):
+            data_dict['y_coordinate'] = row[9]
+        else:
+            data_dict['y_coordinate'] = None
+        if(not math.isnan(row[10])):
+            data_dict['longitude'] = row[10]
+        else:
+            data_dict['longitude'] = None
+        if(not math.isnan(row[11])):
+            data_dict['latitude'] = row[11]
+        else:
+            data_dict['latitude'] = None
 
-    WindFarmDetailData.objects.create(**data_dict)
-    print(f"turbine operator name {row[1]} with sitename {row[2]} inserted")
+        WindFarmDetailData.objects.create(**data_dict)
+        print(f"turbine operator name {row[1]} with sitename {row[2]} inserted")
