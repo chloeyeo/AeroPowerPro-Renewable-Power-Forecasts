e2717d47269aa32e1a4df6d6e8a5fe9e036c80c2
George Ntogramatzis <2518439n@student.gla.ac.uk>
2023-03-15 14:52:40 +0000

    replace absolute path with relative path to avoid cookie interference

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -37,7 +37,7 @@ const Register = () => {
       })
         .then(function (response) {
           event.preventDefault();
-          window.location.replace("http://127.0.0.1:3000");
+          window.location.replace("/");
           console.log(response);
         })
         .catch(function (error) {
